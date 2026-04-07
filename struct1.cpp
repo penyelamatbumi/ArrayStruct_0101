e71f97f86f34bbe5cce4d24be1d16859cc42d547
penyelamatbumi <inihanip7@gmail.com>
2026-04-07 17:26:51 +0700

    menambahkan cout

--- struct1.cpp
+++ struct1.cpp
@@ -55,3 +55,32 @@ int main()
     cout << "Alamat : " << mhs.alamat << endl;
     cout << "Usia : " << mhs.umur << endl;
 }
+
+#include <iostream>
+using namespace std;
+
+struct mahasiswa
+{
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main()
+{
+    // deklarasi variable struct
+    mahasiswa mhs;
+
+    // mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "Isikan data usia : ";
+    cin >> mhs.umur;
+
+    cout <<"Data Mahasiswa" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Usia : " << mhs.umur << endl;
+}
