211c7769ad5beb78a979dc0fa587d59081a1a361
penyelamatbumi <inihanip7@gmail.com>
2026-04-07 17:32:24 +0700

    menambahkan end

--- struct2.cpp
+++ struct2.cpp
@@ -145,3 +145,40 @@ int main()
     cout << "Desa : " << mhs.alamat.desa << endl;
     cout << "Usia : " << mhs.umur << endl;
 }
+
+#include <iostream>
+using namespace std;
+
+struct alamatDetail
+{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+};
+
+int main()
+{
+    // deklarasi variable struct
+    mahasiswa mhs;
+    // mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Isikan data desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Isikan data usia : ";
+    cin >> mhs.umur;
+
+    cout <<"Data Mahasiswa" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Kota : " << mhs.alamat.kota << endl;
+    cout << "Desa : " << mhs.alamat.desa << endl;
+    cout << "Usia : " << mhs.umur << endl;
+}
